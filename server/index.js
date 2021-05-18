830e0d65ab50a21df0a7a4246374a9caf7a63b1a
Justin Carl Velarde <46807325+jcv21@users.noreply.github.com>
2021-05-18 22:53:30 +0800

    routing updates

--- server/index.js
+++ server/index.js
@@ -2,4 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts';
 
+// initialize the express app
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+
+// to remove later this is put here for development purposes
+const CONNECTION_URL = 'mongodb+srv://justin21:python01@database.piorv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// Initialize Mongoose connection
+mongoose
+    .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
