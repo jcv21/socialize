e87fd54e8a75695023c523d54a9c6bf50b09f03c
Justin Carl Velarde <46807325+jcv21@users.noreply.github.com>
2021-05-20 23:18:57 +0800

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line
 export default (posts = [], action) => {
     switch (action.type) {
+        case 'FETCH_ALL':
+            return action.payload;
         case 'DELETE':
             return posts.filter((post) => post._id === action.payload);
         case 'UPDATE':
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'FETCH_ALL':
-            return action.payload;
         case 'CREATE':
             return [...posts, action.payload];            
         default:
