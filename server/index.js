36af58f406cb6408a77ae07eea9fef4eb48c0e20
Justin Carl Velarde <46807325+jcv21@users.noreply.github.com>
2021-05-21 22:37:58 +0800

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+
+// initialize the express app
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+// Always put the routes below the cors code to prevent network errors.
+app.use('/posts', postRoutes);
+
+
+// to remove later this is put here for development purposes
+const PORT = process.env.PORT || 5000;
+
+// Initialize Mongoose connection
+mongoose
+    .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
