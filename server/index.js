2899454f45002c731835fda5bda053aa3c6f48c7
Justin Carl Velarde <46807325+jcv21@users.noreply.github.com>
2021-05-21 22:29:40 +0800

    Environmental Variables

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 
 // initialize the express app
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -18,12 +20,11 @@ app.use('/posts', postRoutes);
 
 
 // to remove later this is put here for development purposes
-const CONNECTION_URL = 'mongodb+srv://justin21:python01@database.piorv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 // Initialize Mongoose connection
 mongoose
-    .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
