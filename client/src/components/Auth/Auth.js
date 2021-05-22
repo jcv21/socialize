2aaa56f65cca82e9f3fad4790010e4a56c23a07a
Justin Carl Velarde <46807325+jcv21@users.noreply.github.com>
2021-05-23 01:03:37 +0800

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+
+import useStyles from './styles';
 
 const Auth = () => {
     return (
